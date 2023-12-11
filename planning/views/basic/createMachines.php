d2b93f43bf5d1eff224cb535f143237c3b71f5c0
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2023-12-11 10:43:56 -0500

    Change text

--- planning/views/basic/createMachines.php
+++ planning/views/basic/createMachines.php
@@ -41,9 +41,9 @@ if (sizeof($_SESSION) == 0)
                         <div class="row align-items-center">
                             <div class="col-sm-5 col-xl-6">
                                 <div class="page-title">
-                                    <h3 class="mb-1 font-weight-bold text-dark">Máquinas</h3>
+                                    <h3 class="mb-1 font-weight-bold text-dark">Máquinas y/o Procesos</h3>
                                     <ol class="breadcrumb mb-3 mb-md-0">
-                                        <li class="breadcrumb-item active">Creación de Máquinas</li>
+                                        <li class="breadcrumb-item active">Creación de Máquinas y/o Procesos</li>
                                     </ol>
                                 </div>
                             </div>
