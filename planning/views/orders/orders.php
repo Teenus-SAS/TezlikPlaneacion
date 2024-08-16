309ec4c10f2f918cbc00118c35579aab5da762a9
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-08-16 14:19:55 -0500

    delete readonly input QP

--- planning/views/orders/orders.php
+++ planning/views/orders/orders.php
@@ -149,7 +149,7 @@ if (sizeof($_SESSION) == 0)
 
                                                 <div class="col-sm-2 floating-label enable-floating-label show-label cardDescription" style="margin-bottom:20px">
                                                     <label for="originalQuantity">Cantidad requerida</label>
-                                                    <input type="text" class="form-control text-center" id="originalQuantity" name="originalQuantity" readonly>
+                                                    <input type="text" class="form-control text-center" id="originalQuantity" name="originalQuantity">
                                                 </div>
 
                                                 <div class="col-sm-5 floating-label enable-floating-label show- mb-3 label">
