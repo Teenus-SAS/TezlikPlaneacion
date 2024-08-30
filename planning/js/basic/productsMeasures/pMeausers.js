2678bcf3bd7eeafc9157e98fad9deda0decc0fa9
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-08-30 15:56:28 -0500

    update last commit inputs products hide

--- planning/js/basic/productsMeasures/pMeausers.js
+++ planning/js/basic/productsMeasures/pMeausers.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
     $(".inputsMeasures").toggle(option === "2", 800);
 
     let optionProductType = $("#idProductType option:selected").text().trim();
-    if (optionProductType != "seleccionar") {
+    if (optionProductType != "Seleccionar") {
       $("#idProductType").change();
     }
   });
