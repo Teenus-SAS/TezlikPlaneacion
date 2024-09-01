534b6aa10b4bc40e31f216a9d871e40c7d72decb
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-08-31 19:52:27 -0500

    sort elements program

--- planning/views/program/programming/programming.php
+++ planning/views/program/programming/programming.php
@@ -69,10 +69,10 @@ if (sizeof($_SESSION) == 0)
                                 <div class="col-sm-7 col-xl-1 form-inline justify-content-sm-end mr-3 cardBottons">
                                     <button class="btn btn-warning" id="btnNewProgramming" name="btnNewProgramming">Programar</button>
                                 </div>
-                                <div class="col-sm-7 col-xl-1 form-inline justify-content-sm-end mr-3 cardSaveBottons">
+                                <div class="col-sm-7 col-xl-1 form-inline justify-content-sm-end cardSaveBottons">
                                     <button class="btn btn-primary" id="btnSavePrograming" name="btnSavePrograming">Guardar</button>
                                 </div>
-                                <div class="col-sm-7 col-xl-1 form-inline justify-content-sm-end mr-3 cardAddOP" style="display: none;">
+                                <div class="col-sm-7 col-xl-2 form-inline cardAddOP" style="display: none;">
                                     <button class="btn btn-info" id="btnAddOP" name="btnAddOP">Crear OP</button>
                                 </div>
                             </div>
