d463ed83b097bef5ea0be40a294f132b02843275
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-09-03 17:30:15 -0500

    update indicators

--- planning/js/dashboard/indicators.js
+++ planning/js/dashboard/indicators.js
@@ -1,6 +1,6 @@
 indicatorsGlobal = () => {
   $.ajax({
-    url: `/api/dashboardIndicators/`,
+    url: `/api/dashboardIndicators`,
     success: function (response) {
       $("#productStockout").html(response[0].newContacts);
     },
