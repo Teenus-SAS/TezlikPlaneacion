668966b84601c32e273aa1d70c9fe0abada0a876
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-09-05 14:00:29 -0500

    Fixed OC Graph

--- planning/js/dashboard/pendingOC.js
+++ planning/js/dashboard/pendingOC.js
@@ -32,29 +32,80 @@ const GraphPendingOC = (data) => {
     datasets.push({
       label: `Estado ${item.status}`,
       data: [item.porcentaje_participacion],
-      backgroundColor:
-        item.status === 2
-          ? "rgba(255, 99, 132, 0.2)"
-          : "rgba(75, 192, 192, 0.2)",
-      borderColor:
-        item.status === 2 ? "rgba(255, 99, 132, 1)" : "rgba(75, 192, 192, 1)",
     });
   });
 
   const ctx = document.getElementById("pendingOCChart").getContext("2d");
-
-  const myChart = new Chart(ctx, {
+  const pendingOCChart = new Chart(ctx, {
+    plugins: [ChartDataLabels],
     type: "bar",
     data: {
-      labels: ["Ejecución Ordenes de Compra"],
-      datasets: datasets,
+      labels: ["Ejecucion Ordenes de Compra"],
+      datasets: [
+        {
+          label: "",
+          data: [data],
+          backgroundColor: "rgba(75, 192, 192, 0.2)", // Color para "Entregado a tiempo"
+          borderColor: "rgba(75, 192, 192, 1)", // Borde para "Entregado a tiempo"
+          borderWidth: 1,
+        },
+        {
+          label: "",
+          data: [100 - data],
+          backgroundColor: "rgba(255, 99, 132, 0.2)", // Color para "No entregado a tiempo"
+          borderColor: "rgba(255, 99, 132, 1)", // Borde para "No entregado a tiempo"
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       maintainAspectRatio: false, // Para usar el tamaño del contenedor y no mantener el ratio
       scales: {
+        x: {
+          stacked: true, // Apilamiento en el eje X
+          title: {
+            display: false,
+            text: "Ordenes de Compra", // Etiqueta para el eje X
+          },
+        },
         y: {
+          stacked: true, // Apilamiento en el eje Y
           beginAtZero: true,
           max: 100,
+          title: {
+            display: true,
+            text: "Porcentaje (%)", // Etiqueta para el eje Y
+          },
+          ticks: {
+            callback: function (value) {
+              return value + "%"; // Agregar el símbolo de porcentaje en el eje Y
+            },
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+        datalabels: {
+          anchor: "center",
+          align: "center",
+          offset: 2,
+          color: "black",
+          font: {
+            size: "12",
+            weight: "normal",
+          },
+          formatter: function (value) {
+            return value + "%"; // Agregar el símbolo de porcentaje a las etiquetas
+          },
+        },
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return tooltipItem.raw + "%"; // Mostrar solo el valor en el tooltip
+            },
+          },
         },
       },
     },
