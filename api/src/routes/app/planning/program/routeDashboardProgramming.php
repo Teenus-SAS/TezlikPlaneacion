249cd5aadcadf66600bec7991041e256892a013f
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-09-06 20:17:53 -0500

    update object dao staff dashboard

--- api/src/routes/app/planning/program/routeDashboardProgramming.php
+++ api/src/routes/app/planning/program/routeDashboardProgramming.php
@@ -7,11 +7,11 @@ $dashboardProgrammingDao = new DashboardProgrammingDao();
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/staffAvailable', function (Request $request, Response $response, $args) use ($dashboardGeneralDao) {
+$app->get('/staffAvailable', function (Request $request, Response $response, $args) use ($dashboardProgrammingDao) {
     session_start();
     $id_company = $_SESSION['id_company'];
 
-    $staffAvailable = $dashboardGeneralDao->findStaffAvailableByCompany($id_company);
+    $staffAvailable = $dashboardProgrammingDao->findStaffAvailableByCompany($id_company);
 
     $response->getBody()->write(json_encode($staffAvailable, JSON_NUMERIC_CHECK));
     return $response->withHeader('Content-Type', 'application/json');
