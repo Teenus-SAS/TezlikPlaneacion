d59d3155c1995c9977be592d9011fd7b87b23e44
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-09-06 20:33:18 -0500

    add chart staff dashboard

--- planning/js/program/dashboard/staffAvailable.js
+++ planning/js/program/dashboard/staffAvailable.js
@@ -25,78 +25,31 @@ $(document).ready(function () {
   };
 });
 
-const GraphStaffAvailable = (percentageOnTime) => {
+const GraphStaffAvailable = (data) => {
+  // Datos del gráfico (asumiendo que los datos están en una variable llamada 'data')
+  const labels = data.map((item) => `${item.process} - ${item.machine}`);
+  const values = data.map((item) => item.total_operadores);
+
+  // Configuración del gráfico
   const ctx = document.getElementById("staffAvailableChart").getContext("2d");
-  const staffAvailableChart = new Chart(ctx, {
-    plugins: [ChartDataLabels],
+  const myChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels: ["Pedidos"],
+      labels: labels,
       datasets: [
         {
-          label: "",
-          data: [percentageOnTime],
-          backgroundColor: "rgba(75, 192, 192, 0.2)", // Color para "Entregado a tiempo"
-          borderColor: "rgba(75, 192, 192, 1)", // Borde para "Entregado a tiempo"
-          borderWidth: 1,
-        },
-        {
-          label: "",
-          data: [100 - percentageOnTime],
-          backgroundColor: "rgba(255, 99, 132, 0.2)", // Color para "No entregado a tiempo"
-          borderColor: "rgba(255, 99, 132, 1)", // Borde para "No entregado a tiempo"
+          label: "Total de Operadores por Proceso y Máquina",
+          data: values,
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgba(54, 162, 235, 1)",
           borderWidth: 1,
         },
       ],
     },
     options: {
-      maintainAspectRatio: false, // Para usar el tamaño del contenedor y no mantener el ratio
       scales: {
-        x: {
-          stacked: true, // Apilamiento en el eje X
-          title: {
-            display: false,
-            text: "Pedidos", // Etiqueta para el eje X
-          },
-        },
         y: {
-          stacked: true, // Apilamiento en el eje Y
           beginAtZero: true,
-          max: 100,
-          title: {
-            display: true,
-            text: "Porcentaje (%)", // Etiqueta para el eje Y
-          },
-          ticks: {
-            callback: function (value) {
-              return value + "%"; // Agregar el símbolo de porcentaje en el eje Y
-            },
-          },
-        },
-      },
-      plugins: {
-        legend: {
-          display: false,
-        },
-        datalabels: {
-          anchor: "center",
-          align: "center",
-          offset: 2,
-          color: "black",
-          font: {
-            size: "12",
-            weight: "normal",
-          },
-          formatter: function (value) {
-            return value + "%"; // Agregar el símbolo de porcentaje a las etiquetas
-          },
-        },
-        tooltip: {
-          callbacks: {
-            label: function (tooltipItem) {
-              return tooltipItem.raw + "%"; // Mostrar solo el valor en el tooltip
-            },
-          },
         },
       },
     },
