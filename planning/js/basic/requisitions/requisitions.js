5849a0e6b4214ab7d503b31cd726684a7fa44296
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-09-28 18:25:11 -0500

    update

--- planning/js/basic/requisitions/requisitions.js
+++ planning/js/basic/requisitions/requisitions.js
@@ -220,7 +220,7 @@ $(document).ready(function () {
   });
 
   const checkDataRequisition = async (url, idRequisition) => {
-    const material = $("#materialName").val();
+    const material = $("#material").val();
     const provider = $("#client").val();
     const applicationDate = $("#applicationDate").val();
     const deliveryDate = $("#deliveryDate").val();
