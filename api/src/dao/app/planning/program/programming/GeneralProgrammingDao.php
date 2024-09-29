c3579b4dc18e0676a813bbb934f319cd23dfa1c7
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-09-28 19:48:17 -0500

    update consult mysql

--- api/src/dao/app/planning/program/programming/GeneralProgrammingDao.php
+++ api/src/dao/app/planning/program/programming/GeneralProgrammingDao.php
@@ -49,51 +49,23 @@ class GeneralProgrammingDao
     public function findAllProgrammingByCompany($id_company)
     {
         $connection = Connection::getInstance()->getConnection();
-
-        $stmt = $connection->prepare("SELECT
-                                        -- Columnas
-                                            pg.id_programming,
-                                            o.id_order,
-                                            o.num_order,
-                                            o.date_order,
-                                            o.original_quantity AS quantity_order,
-                                            o.accumulated_quantity,
-                                            pg.quantity AS quantity_programming,
-                                            p.id_product,
-                                            p.reference,
-                                            p.product,
-                                            m.id_machine,
-                                            m.machine,
-                                            c.client,
-                                            pg.min_date,
-                                            HOUR(pg.min_date) AS min_hour,
-                                            pm.hour_start,
-                                            pg.max_date,
-                                            HOUR(pg.max_date) AS max_hour,
-                                            (
-                                                SELECT
-                                                    IFNULL(
-                                                        (1 * cm.quantity / cpm.quantity),
-                                                        0
-                                                    )
-                                                FROM
-                                                    products_materials cpm
-                                                INNER JOIN inv_materials cm ON
-                                                    cm.id_material = cpm.id_material
-                                                WHERE
-                                                    cpm.id_product = pg.id_product
-                                                ORDER BY
-                                                    `IFNULL((1*cm.quantity/cpm.quantity), 0)` ASC
-                                                LIMIT 1
-                                            ) AS quantity_mp, 
-                                            pg.status
-                                        FROM programming pg
-                                            INNER JOIN orders o ON o.id_order = pg.id_order
-                                            INNER JOIN products p ON p.id_product = pg.id_product
-                                            INNER JOIN machines m ON m.id_machine = pg.id_machine
-                                            INNER JOIN third_parties c ON c.id_client = o.id_client
-                                            INNER JOIN machine_programs pm ON pm.id_machine = pg.id_machine
-                                        WHERE pg.id_company = :id_company AND pg.status = 1");
+        $sql = "SELECT pg.id_programming, o.id_order, o.num_order, o.date_order, o.original_quantity AS quantity_order, 
+                        o.accumulated_quantity, pg.quantity AS quantity_programming, p.id_product, p.reference, p.product, 
+                        m.id_machine, m.machine, c.client, pg.min_date, HOUR(pg.min_date) AS min_hour, pm.hour_start, 
+                        pg.max_date, HOUR(pg.max_date) AS max_hour, 
+                        (SELECT COALESCE((1 * cm.quantity / cpm.quantity), 0) AS quantity_ratio
+                            FROM products_materials cpm
+                            INNER JOIN inv_materials cm ON cm.id_material = cpm.id_material
+                            WHERE cpm.id_product = pg.id_product ORDER BY 1 ASC LIMIT 1
+                            ) AS quantity_mp, pg.status
+                FROM programming pg
+                INNER JOIN orders o ON o.id_order = pg.id_order
+                INNER JOIN products p ON p.id_product = pg.id_product
+                INNER JOIN machines m ON m.id_machine = pg.id_machine
+                INNER JOIN third_parties c ON c.id_client = o.id_client
+                INNER JOIN machine_programs pm ON pm.id_machine = pg.id_machine
+                WHERE pg.id_company = :id_company AND pg.status = 1;";
+        $stmt = $connection->prepare($sql);
         $stmt->execute(['id_company' => $id_company]);
 
         $this->logger->info(__FUNCTION__, array('query' => $stmt->queryString, 'errors' => $stmt->errorInfo()));
