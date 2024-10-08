7dc84ed97a9b6c3788cad82c9b562523a9b5c341
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-10-08 16:47:33 -0500

    update scripts

--- global/partials/scriptsJS.php
+++ global/partials/scriptsJS.php
@@ -1,3 +1,59 @@
+<script>
+    // Seleccionar todos los enlaces y sus respectivos iconos
+    const links = [{
+            link: document.getElementById("dashboard-link"),
+            icon: document.getElementById("dashboard-icon")
+        },
+        {
+            link: document.getElementById("inventory-link"),
+            icon: document.getElementById("inventory-icon")
+        },
+        {
+            link: document.getElementById("requisitions-link"),
+            icon: document.getElementById("requisitions-icon")
+        },
+        {
+            link: document.getElementById("orders-link"),
+            icon: document.getElementById("orders-icon")
+        },
+        {
+            link: document.getElementById("programming-link"),
+            icon: document.getElementById("programming-icon")
+        },
+        {
+            link: document.getElementById("explosion-link"),
+            icon: document.getElementById("explosion-icon")
+        },
+        {
+            link: document.getElementById("production-order-link"),
+            icon: document.getElementById("production-order-icon")
+        },
+        {
+            link: document.getElementById("store-link"),
+            icon: document.getElementById("store-icon")
+        },
+        {
+            link: document.getElementById("offices-link"),
+            icon: document.getElementById("offices-icon")
+        }
+    ];
+
+    // Agregar evento click a cada enlace
+    links.forEach((item) => {
+        item.link.addEventListener("click", () => {
+            // Cambiar el color del ícono seleccionado
+            item.icon.style.color = "yellow"; // Cambia el color del icono del enlace seleccionado
+
+            // Cambiar el color de los demás iconos a su color original (gris)
+            links.forEach((resetItem) => {
+                if (resetItem !== item) {
+                    resetItem.icon.style.color = ""; // Restablecer color predeterminado
+                }
+            });
+        });
+    });
+</script>
+
 <!-- jquery -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <!-- ================== BEGIN BASE JS ================== -->
@@ -71,59 +127,3 @@
 <!-- <script src="/global/js/global/number.js"></script> -->
 <script src="/global/js/global/lastText.js"></script>
 <script src="/global/js/global/convertDate.js"></script>
-
-<script>
-    // Seleccionar todos los enlaces y sus respectivos iconos
-    const links = [{
-            link: document.getElementById("dashboard-link"),
-            icon: document.getElementById("dashboard-icon")
-        },
-        {
-            link: document.getElementById("inventory-link"),
-            icon: document.getElementById("inventory-icon")
-        },
-        {
-            link: document.getElementById("requisitions-link"),
-            icon: document.getElementById("requisitions-icon")
-        },
-        {
-            link: document.getElementById("orders-link"),
-            icon: document.getElementById("orders-icon")
-        },
-        {
-            link: document.getElementById("programming-link"),
-            icon: document.getElementById("programming-icon")
-        },
-        {
-            link: document.getElementById("explosion-link"),
-            icon: document.getElementById("explosion-icon")
-        },
-        {
-            link: document.getElementById("production-order-link"),
-            icon: document.getElementById("production-order-icon")
-        },
-        {
-            link: document.getElementById("store-link"),
-            icon: document.getElementById("store-icon")
-        },
-        {
-            link: document.getElementById("offices-link"),
-            icon: document.getElementById("offices-icon")
-        }
-    ];
-
-    // Agregar evento click a cada enlace
-    links.forEach((item) => {
-        item.link.addEventListener("click", () => {
-            // Cambiar el color del ícono seleccionado
-            item.icon.style.color = "yellow"; // Cambia el color del icono del enlace seleccionado
-
-            // Cambiar el color de los demás iconos a su color original (gris)
-            links.forEach((resetItem) => {
-                if (resetItem !== item) {
-                    resetItem.icon.style.color = ""; // Restablecer color predeterminado
-                }
-            });
-        });
-    });
-</script>
