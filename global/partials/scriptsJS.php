2e7549747ddb8c61f3ceca2c121a1edbf2557e1f
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-10-08 16:49:15 -0500

    update

--- global/partials/scriptsJS.php
+++ global/partials/scriptsJS.php
@@ -26,7 +26,7 @@
 
 <!-- Datatables -->
 <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-<!-- <script src="https://cdn.datatables.net/2.1.7/js/dataTables.min.js"></script> -->
+ <!-- <script src="https://cdn.datatables.net/2.1.7/js/dataTables.min.js"></script> -->
 <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.datatables.net/rowgroup/1.2.0/js/dataTables.rowGroup.min.js"></script>
 <script src="https://cdn.datatables.net/rowreorder/1.4.1/js/dataTables.rowReorder.min.js"></script>
