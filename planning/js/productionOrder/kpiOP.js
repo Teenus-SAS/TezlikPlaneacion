fa1bf323d8af1ba4f3a0a12d77e6bf185255dad8
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-10-09 18:50:52 -0500

    update data

--- planning/js/productionOrder/kpiOP.js
+++ planning/js/productionOrder/kpiOP.js
@@ -1,21 +1,19 @@
-$(document).ready(function () {
-  // Esperar hasta que el DataTable esté listo
-  const checkDataTableReady = setInterval(() => {
-    if (localStorage.getItem("dataTableReady") === "true") {
-      // El DataTable está listo, ahora podemos acceder a las variables almacenadas en localStorage
-      const totalDefectiveUnits =
-        parseFloat(localStorage.getItem("totalDefectiveUnits")) || 0;
-      const totalDeliveredQuantity =
-        parseFloat(localStorage.getItem("totalDeliveredQuantity")) || 0;
+// Esperar hasta que el DataTable esté listo
+const checkDataTableReady = setInterval(() => {
+  if (localStorage.getItem("dataTableReady") === "true") {
+    // El DataTable está listo, ahora podemos acceder a las variables almacenadas en localStorage
+    const totalDefectiveUnits =
+      parseFloat(localStorage.getItem("totalDefectiveUnits")) || 0;
+    const totalDeliveredQuantity =
+      parseFloat(localStorage.getItem("totalDeliveredQuantity")) || 0;
 
-      // Realizar otras operaciones o pasar los datos a otras funciones según sea necesario
+    // Realizar otras operaciones o pasar los datos a otras funciones según sea necesario
 
-      console.log("Total Unidades Defectuosas:", totalDefectiveUnits);
-      console.log("Total Cantidad Entregada:", totalDeliveredQuantity);
-      let quality = totalDefectiveUnits / totalDeliveredQuantity;
-      $("#kpiQualityOP").val(quality);
-      // Detener el setInterval ya que el DataTable está listo
-      clearInterval(checkDataTableReady);
-    }
-  }, 500); // Verificar cada 500 ms si el DataTable está listo
-});
+    console.log("Total Unidades Defectuosas:", totalDefectiveUnits);
+    console.log("Total Cantidad Entregada:", totalDeliveredQuantity);
+    let quality = totalDefectiveUnits / totalDeliveredQuantity;
+    $("#kpiQualityOP").val(quality);
+    // Detener el setInterval ya que el DataTable está listo
+    clearInterval(checkDataTableReady);
+  }
+}, 500); // Verificar cada 500 ms si el DataTable está listo
