1f987afb5d404cbc4fc19aac1d87d952385492e1
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-10-10 11:49:13 -0500

    update kpi QC

--- planning/js/productionOrder/kpiOP.js
+++ planning/js/productionOrder/kpiOP.js
@@ -1,39 +1,38 @@
 $(document).ready(function () {
-  // Extraer valores de unidades defectuosas y procesadas, eliminando la palabra "Und"
-  let unitsDefects =
-    parseFloat(
-      $(".unitsDefects")
-        .text()
-        .replace(" Und", "")
-        .replace(".", "")
-        .replace(",", ".")
-    ) || 0;
-  let unitsProcessing =
-    parseFloat(
-      $(".unitsProcessing")
-        .text()
-        .replace(" Und", "")
-        .replace(".", "")
-        .replace(",", ".")
-    ) || 0;
+  // Seleccionar los elementos <th> a observar
+  const targetDefects = document.querySelector(".unitsDefects");
+  const targetProcessing = document.querySelector(".unitsProcessing");
 
-  // Verificar que los valores de 'unitsDefects' y 'unitsProcessing' sean válidos
-  if (unitsDefects > 0 && unitsProcessing > 0) {
-    // Calcular la calidad del proceso (porcentaje)
-    let quality = ((unitsProcessing - unitsDefects) / unitsProcessing) * 100;
+  // Crear una instancia de MutationObserver
+  const observer = new MutationObserver(function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        console.log(
+          "Contenido de las columnas .unitsDefects o .unitsProcessing cambió."
+        );
 
-    // Mostrar el resultado con dos decimales en el elemento correspondiente
-    $("#kpiQualityOP").text(`Q: ${quality.toFixed(2)}%`);
-  } else {
-    // Ocultar el elemento si los valores no son válidos
-    $("#kpiQualityOP").hide();
-  }
-  /* $.ajax({
-    url: "/api/kpisOP",
-    data: "data",
-    success: function (data) {
-      if (data) $("#kpiQualityOP").text(`Q: ${data.toFixed(2)}%`);
-      else $("#kpiQualityOP").hide();
-    },
-  }); */
+        // Capturar los nuevos valores
+        const unitsDefects = parseFloat(targetDefects.textContent) || 0;
+        const unitsProcessing = parseFloat(targetProcessing.textContent) || 0;
+
+        // Realizar el cálculo del KPI de calidad
+        if (unitsProcessing > 0) {
+          const quality =
+            ((unitsProcessing - unitsDefects) / unitsProcessing) * 100;
+          $("#kpiQualityOP")
+            .text(`QC: ${quality.toFixed(2)}%`)
+            .show();
+        } else {
+          $("#kpiQualityOP").hide();
+        }
+      }
+    }
+  });
+
+  // Opciones de configuración para observar los cambios en el contenido de los elementos
+  const config = { childList: true };
+
+  // Iniciar la observación en las columnas específicas
+  if (targetDefects) observer.observe(targetDefects, config);
+  if (targetProcessing) observer.observe(targetProcessing, config);
 });
