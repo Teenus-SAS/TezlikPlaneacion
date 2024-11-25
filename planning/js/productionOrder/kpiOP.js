12baaaada9f10ba0304deab4f6029c4136876bfa
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-11-24 21:04:24 -0500

    update data kpi op

--- planning/js/productionOrder/kpiOP.js
+++ planning/js/productionOrder/kpiOP.js
@@ -1,139 +1,77 @@
 $(document).ready(function () {
   // Seleccionar los elementos <th> a observar
-  const targetDefects = document.querySelector(".unitsDefects");
-  const targetProcessing = document.querySelector(".unitsProcessing");
-  const targetCostPayroll = document.querySelector(".costPayroll");
-  const targetCostMaterials = document.querySelector(".costMaterials");
-  const targetCostIndirect = document.querySelector(".costIndirect");
+  const targets = {
+    defects: document.querySelector(".unitsDefects"),
+    processing: document.querySelector(".unitsProcessing"),
+    payroll: document.querySelector(".costPayroll"),
+    materials: document.querySelector(".costMaterials"),
+    indirect: document.querySelector(".costIndirect"),
+  };
 
   // Verifica si los elementos existen antes de iniciar el observador
-  if (targetDefects && targetProcessing && targetCostPayroll) {
+  if (Object.values(targets).every((target) => target)) {
     // Crear una instancia de MutationObserver
     const observer = new MutationObserver(function (mutationsList) {
       mutationsList.forEach((mutation) => {
         if (mutation.type === "childList") {
-          console.log(
-            "Contenido de las columnas .unitsDefects, .unitsProcessing o .costPayroll cambió."
-          );
+          console.log("Se detectaron cambios en las columnas observadas.");
+
+          // Funciones para limpiar y formatear valores
+          const parseCurrency = (value) =>
+            parseFloat(value.replace(/[\$\.]/g, "").replace(",", ".")) || 0;
+
+          const formatCurrency = (value) =>
+            `$${value.toLocaleString("es-CO", {
+              maximumFractionDigits: 2,
+            })}`;
 
           // Capturar los nuevos valores
           const unitsDefects =
-            parseFloat(
-              $(".unitsDefects")
-                .text()
-                .replace(" Und", "")
-                .replace(".", "")
-                .replace(",", ".")
-            ) || 0;
-          const unitsProcessing =
-            parseFloat(
-              $(".unitsProcessing")
-                .text()
-                .replace(" Und", "")
-                .replace(".", "")
-                .replace(",", ".")
-            ) || 0;
-          const costPayroll =
-            parseFloat(
-              $(".costPayroll")
-                .text()
-                .replace("$", "")
-                .replace(".", "")
-                .replace(",", ".")
-            ) || 0;
-          const costMaterials =
-            parseInt($(".costMaterials").replace(/[\$\.]/g, ""), 10) || 0;
-          /* parseFloat(
-            $(".costMaterials")
-              .text()
-              .replace("$", "")
-              .replace(".", "")
-              .replace(",", ".")
-          ) || 0; */
-          const costIndirect =
-            parseFloat(
-              $(".costIndirect")
-                .text()
-                .replace("$", "")
-                .replace(".", "")
-                .replace(",", ".")
-            ) || 0;
-          const costMaterialsUnit =
-            parseFloat(
-              $(".costMaterialsUnit")
-                .text()
-                .replace("$", "")
-                .replace(".", "")
-                .replace(",", ".")
-            ) || 0;
+            parseFloat($(".unitsDefects").text().replace(" Und", "")) || 0;
 
-          // Realizar el cálculo del KPI de calidad
+          const unitsProcessing =
+            parseFloat($(".unitsProcessing").text().replace(" Und", "")) || 0;
+
+          const costPayroll = parseCurrency($(".costPayroll").text());
+          const costMaterials = parseCurrency($(".costMaterials").text());
+          const costIndirect = parseCurrency($(".costIndirect").text());
+
           if (unitsProcessing > 0) {
-            //Total Cost
+            // Calcular costos y KPI
             let totalCost = costPayroll + costMaterials + costIndirect;
-            let costMPUnit = costMaterials / unitsProcessing;
             let costPayrollUnit = costPayroll / unitsProcessing;
-            let costIndirectunit = costIndirect / unitsProcessing;
-            let totalCostUnit = costPayrollUnit + costIndirectunit;
-            //calc quality
-            let quality = 1 - unitsDefects / unitsProcessing;
-            quality = quality * 100;
+            let costMaterialsUnit = costMaterials / unitsProcessing;
+            let costIndirectUnit = costIndirect / unitsProcessing;
+            let totalCostUnit =
+              costPayrollUnit + costMaterialsUnit + costIndirectUnit;
+
+            let quality = (1 - unitsDefects / unitsProcessing) * 100;
+
+            // Actualizar valores en la interfaz
             $("#kpiQualityOP")
               .text(`QC: ${quality.toFixed(2)}%`)
               .show();
-
+            $("#kpiCostPayroll").text(`MO: ${formatCurrency(costPayroll)}`);
             $("#kpiCostPayrollUnit").text(
-              `MO: $${costPayrollUnit.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
+              `MO: ${formatCurrency(costPayrollUnit)}`
             );
-
-            $("#kpiCostPayroll").text(
-              `MO: $${costPayroll.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
-            );
-
-            $("#kpiCostMaterials").text(
-              `MP: $${costMaterials.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
-            );
+            $("#kpiCostMaterials").text(`MP: ${formatCurrency(costMaterials)}`);
             $("#kpiCostMaterialsUnit").text(
-              `MP: $${costMPUnit.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
+              `MP: ${formatCurrency(costMaterialsUnit)}`
             );
-
-            $("#kpiIndirectCost").text(
-              `CI: $${costIndirect.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
-            );
+            $("#kpiIndirectCost").text(`CI: ${formatCurrency(costIndirect)}`);
             $("#kpiIndirectCostUnit").text(
-              `CI: $${costIndirectunit.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
+              `CI: ${formatCurrency(costIndirectUnit)}`
             );
+            $("#kpiTotalCost").text(`CT: ${formatCurrency(totalCost)}`);
+            $("#kpiTotalCostUnit").text(`CT: ${formatCurrency(totalCostUnit)}`);
 
-            $("#kpiTotalCost").text(
-              `CT: $${totalCost.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
-            );
-            $("#kpiTotalCostUnit").text(
-              `CT: $${totalCostUnit.toLocaleString("es-CO", {
-                maximumFractionDigits: 2,
-              })}`
-            );
-            $("#titleGeneralCost").show();
-            $("#titleUnitCost").show();
-            $("#lineDivTitleCost").show();
+            // Mostrar títulos y divisores
+            $("#titleGeneralCost, #titleUnitCost, #lineDivTitleCost").show();
           } else {
+            // Ocultar KPI si no hay unidades procesadas
             $("#kpiQualityOP").hide();
-            $("#titleGeneralCost").hide();
-            $("#titleUnitCost").hide();
-            $("#lineDivTitleCost").hide();
+            $("#titleGeneralCost, #titleUnitCost, #lineDivTitleCost").hide();
           }
         }
       });
@@ -143,11 +81,9 @@ $(document).ready(function () {
     const config = { childList: true, subtree: true };
 
     // Iniciar la observación en las columnas específicas
-    observer.observe(targetDefects, config);
-    observer.observe(targetProcessing, config);
-    observer.observe(targetCostPayroll, config);
-    observer.observe(targetCostMaterials, config);
-    observer.observe(targetCostIndirect, config);
+    Object.values(targets).forEach((target) =>
+      observer.observe(target, config)
+    );
   } else {
     console.error("No se encontraron los elementos para observar.");
   }
